c95ffa2ca840fee49ea9686a5ffeb2fcde6da0d2
Soumahoro-Aboubakar <AboubakarKone8958@gmail.com>
2024-05-26 12:18:36 -0300

    app deploy

---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,77 @@
+import { useEffect, useState } from "react";
+import PortfolioList from "../portfolioList/PortfolioList";
+import "./portfolio.scss";
+import {
+  featuredPortfolio,
+  webPortfolio,
+  mobilePortfolio,
+  backendPortoflio,
+} from "../../data";
+
+export default function Portfolio() {
+  const [selected, setSelected] = useState("featured");
+  const [data, setData] = useState([]);
+  const list = [
+    {
+      id: "featured",
+      title: "Featured",
+    },
+    {
+      id: "web",
+      title: "Web App",
+    },
+    {
+      id: "mobile",
+      title: "Mobile App",
+    },
+    {
+      id: "backend",
+      title: "Backend side",
+    },
+  ];
+
+  useEffect(() => {
+    switch (selected) {
+      case "featured":
+        setData(featuredPortfolio);
+        break;
+      case "web":
+        setData(webPortfolio);
+        break;
+      case "mobile":
+        setData(mobilePortfolio);
+        break;
+      case "backend":
+        setData(backendPortoflio);
+        break;
+      default:
+        setData(featuredPortfolio);
+    }
+  }, [selected]);
+
+  return (
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        {list.map((item) => (
+          <PortfolioList
+            title={item.title}
+            active={selected === item.id}
+            setSelected={setSelected}
+            id={item.id}
+          />
+        ))}
+      </ul>
+      <div className="container">
+        {data.map((d) => (
+          <a href={d.url} target="_blank" rel="noopener noreferrer">
+            <div className="item">
+              <img src={d.img} alt="" />
+              <h3>{d.title}</h3>
+            </div>
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,140 @@
+import {
+  WebPortfolio_BestPortfolio,
+  mobilePortfolio_QuizAPP,
+  mobilePortfolio_WebVIEWAPP,
+  webPortfolio_UserProfile,
+} from "./components/Constants";
+
+export const featuredPortfolio = [
+  {
+    id: 1,
+    title: "Social Media App",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQACRJUGmlvx--sKuIui1IOQVniJ0ZjRbgJEA&s",
+    url: "https://github.com/Soumahoro-Aboubakar/Social-Media",
+  },
+  {
+    id: 2,
+    title: "Social Media App",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNMiRN1A_2cy5dmukB_TrhmnZdoyz2yzNr7g&s",
+    url: "https://github.com/Soumahoro-Aboubakar/Social-Media",
+  },
+  {
+    id: 3,
+    title: "Social Media App",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNMiRN1A_2cy5dmukB_TrhmnZdoyz2yzNr7g&s",
+    url: "https://github.com/Soumahoro-Aboubakar/Social-Media",
+  },
+  {
+    id: 4,
+    title: "Best Portfolio",
+    url: "https://github.com/Soumahoro-Aboubakar/BestPortfolio",
+    img: WebPortfolio_BestPortfolio,
+  },
+  {
+    id: 5,
+    title: "User profile",
+    url: "https://github.com/Soumahoro-Aboubakar/userProfile",
+    img: webPortfolio_UserProfile,
+  }, {
+    id: 6,
+    title: "QUIZ App",
+    url: "https://github.com/Soumahoro-Aboubakar/Quiz-APP",
+    img: mobilePortfolio_QuizAPP,
+  },
+  {
+    id: 7,
+    title: "WEBVIEW APP",
+    url: "https://github.com/Soumahoro-Aboubakar/WebViewApp",
+    img: mobilePortfolio_WebVIEWAPP,
+  },
+];
+
+export const webPortfolio = [
+  {
+    id: 1,
+    title: "Web Social Media App",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQACRJUGmlvx--sKuIui1IOQVniJ0ZjRbgJEA&s",
+    url: "https://github.com/Soumahoro-Aboubakar/Social-Media",
+  },
+  {
+    id: 2,
+    title: "Best Portfolio",
+    url: "https://github.com/Soumahoro-Aboubakar/BestPortfolio",
+    img: WebPortfolio_BestPortfolio,
+  },
+  {
+    id: 3,
+    title: "User profile",
+    url: "https://github.com/Soumahoro-Aboubakar/userProfile",
+    img: webPortfolio_UserProfile,
+  },
+];
+
+export const mobilePortfolio = [
+  {
+    id: 1,
+    title: "QUIZ App",
+    url: "https://github.com/Soumahoro-Aboubakar/Quiz-APP",
+    img: mobilePortfolio_QuizAPP,
+  },
+  {
+    id: 2,
+    title: "WEBVIEW APP",
+    url: "https://github.com/Soumahoro-Aboubakar/WebViewApp",
+    img: mobilePortfolio_WebVIEWAPP,
+  },
+];
+
+export const backendPortoflio = [
+  {
+    id: 1,
+    title: "Social Media App",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQACRJUGmlvx--sKuIui1IOQVniJ0ZjRbgJEA&s",
+    url: "https://github.com/Soumahoro-Aboubakar/Social-Media",
+  },
+  {
+    id: 2,
+    title: "Social Media App",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNMiRN1A_2cy5dmukB_TrhmnZdoyz2yzNr7g&s",
+    url: "https://github.com/Soumahoro-Aboubakar/Social-Media",
+  },
+  {
+    id: 3,
+    title: "Social Media App",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNMiRN1A_2cy5dmukB_TrhmnZdoyz2yzNr7g&s",
+    url: "https://github.com/Soumahoro-Aboubakar/Social-Media",
+  },
+];
+
+export const contentPortfolio = [
+  {
+    id: 1,
+    title: "Content Social Media App",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+  },
+  {
+    id: 2,
+    title: "Content Rampa UI Design",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+  },
+  {
+    id: 3,
+    title: "Content E-commerce Web Design",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+  },
+  {
+    id: 4,
+    title: "Content Relax Mobile App",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+  },
+  {
+    id: 5,
+    title: "Content Keser Web Design",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+  },
+  {
+    id: 6,
+    title: "Content Banking App",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+  },
+];
